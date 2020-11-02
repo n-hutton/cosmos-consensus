8b017ed9588edadbe08b53418404d1e8569755a9
nhutton <nathan.hutton@fetch.ai>
2020-11-02 07:33:22 +0000

    Revert "fix public check keys"
    This reverts commit 7a3e89e31add35390956e832b5ace0f7eba4b519.

--- mcl_cpp/aeon_exec_unit.hpp
+++ mcl_cpp/aeon_exec_unit.hpp
@@ -51,7 +51,6 @@ public:
   virtual bool CheckIndex(CabinetIndex index) const = 0;
   virtual Signature ComputeGroupSignature(std::map <CabinetIndex, Signature> const &signature_shares) const = 0;
   virtual std::string Name() const = 0;
-  virtual void CheckKeys() const = 0;
 
   bool VerifyGroupSignature(MessagePayload const &message, Signature const &signature) const;
 
@@ -73,6 +72,8 @@ protected:
   explicit BaseAeon(std::string const &filename);
   BaseAeon(DKGKeyInformation const &keys, std::vector<CabinetIndex> const &qual); 
   BaseAeon(std::string generator, DKGKeyInformation keys, std::set<CabinetIndex> qual);
+
+  virtual void CheckKeys() const = 0;
 };
 
 class BlsAeon : public BaseAeon {
@@ -88,6 +89,8 @@ public:
   Signature ComputeGroupSignature(std::map <CabinetIndex, Signature> const &signature_shares) const override;
   bool CheckIndex(CabinetIndex index) const override;
   std::string Name() const override;
+
+private:
   void CheckKeys() const override;
 };
 
@@ -104,10 +107,11 @@ public:
   bool CheckIndex(CabinetIndex index) const override;
   std::string Generator() const override;
   std::string Name() const override;
-  void CheckKeys() const override;
 
 private:
   std::string generator_g1_;
+
+  void CheckKeys() const override;
 };
 
 }  // namespace crypto
